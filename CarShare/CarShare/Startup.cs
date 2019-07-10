9d281a7981e60155fc311010f33e918b27eee409
Kylian Chaudemanche <kylian.chaudemanche@gmail.com>
2019-07-10 16:11:05 +0200

    Initial Commit

---
+++ CarShare/CarShare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarShare.Startup))]
+namespace CarShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
