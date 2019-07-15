708129d8b1a624744f6bbfce4c293506cf43e88c
MAXENCE MILLOT <maxence.millot2018@campus-eni.fr>
2019-07-15 16:36:55 +0200

    Ajout liens navbar

--- CarShare/CarShare/Controllers/ArretsController.cs
+++ CarShare/CarShare/Controllers/ArretsController.cs
@@ -47,7 +47,7 @@ namespace CarShare.Controllers
         // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Horaire")] Arret arret)
+        public ActionResult Create([Bind(Include = "Id,Horaire,EtatArret")] Arret arret)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace CarShare.Controllers
         // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Horaire")] Arret arret)
+        public ActionResult Edit([Bind(Include = "Id,Horaire,EtatArret")] Arret arret)
         {
             if (ModelState.IsValid)
             {
