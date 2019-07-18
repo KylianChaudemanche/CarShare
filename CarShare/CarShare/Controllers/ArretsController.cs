81da13dcb3a6c525f3253e092d9b7b190f3e2f95
Aurelie RUAULT <aurelie.ruault2017@campus-eni.fr>
2019-07-18 13:37:25 +0200

    Début gestion arrêts

--- CarShare/CarShare/Controllers/ArretsController.cs
+++ CarShare/CarShare/Controllers/ArretsController.cs
@@ -8,6 +8,9 @@ using System.Web;
 using System.Web.Mvc;
 using CarShare.BO;
 using CarShare.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
 
 namespace CarShare.Controllers
 {
@@ -53,6 +56,20 @@ namespace CarShare.Controllers
             {
                 db.Arrets.Add(arret);
                 db.SaveChanges();
+                Task.Factory.StartNew(() =>
+                {
+                    var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+                    var client = new SendGridClient(apiKey);
+                    var from = new EmailAddress("test@example.com", "Example User");
+                    var subject = "Sending with Twilio SendGrid is Fun";
+                    var to = new EmailAddress("aurelie1032@yahoo.fr", "Example User");
+                    var plainTextContent = "and easy to do anywhere, even with C#";
+                    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+                    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                    var response = client.SendEmailAsync(msg);
+
+                });
+                   
                 return RedirectToAction("Index");
             }
 
@@ -124,5 +141,6 @@ namespace CarShare.Controllers
             }
             base.Dispose(disposing);
         }
+
     }
 }
---
+++ CarShare/CarShare/Models/ArretsViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using CarShare.BO;
+
+namespace CarShare.Models
+{
+    public class ArretsViewModels
+    {
+        public Arret Arret { get; set; }
+
+        public Trajet Trajet { get; set; }
+
+        public Emplacement Emplacement { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Horaire { get; set; }
+
+        [Display(Name = "Etat de l'arrêt")]
+        [Range(1,3)]
+        public int EtatArret { get; set; }
+
+
+    }
+}
